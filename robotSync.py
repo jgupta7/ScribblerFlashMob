db1e66ed4e65ccec1f96aedb73a0801c64ae42ff
jgupta7 <jgupta7@gatech.edu>
2014-10-05 16:22:42 -0400

    Removed Old Condition Added Move Function
    Added basic dance moves and in the process of changing conditionals from
    using computer time to downloading from a url

--- robotSync.py
+++ robotSync.py
@@ -4,15 +4,19 @@
 from Myro import *
 
 import datetime
+import urllib
 import ramblin.py
 import ramblin2.py
 
 #init()
 songOrDance = "dance"
 isComplete = True
+
 while isComplete:
-    currMinute = datetime.datetime.now().time().minute
-    if (currMinute == 25):
+    #currMinute = datetime.datetime.now().time().minute
+    #if (currMinute == 25):
+    #old condition ^ where we use computer time
+    if (words):
         #play dat beat
         #do dat dance
         if (songOrDance == "dance"):
@@ -22,3 +26,14 @@ while isComplete:
             ramblin.ramblin()
             #ramblin2.ramblin()
         isComplete = False
+
+
+def backAndForth():
+    forward(1,0.5)
+    backward(1,0.5)
+def spinLeftRight():
+
+def moveTurnRight():
+
+def moveTurnLeft():
+

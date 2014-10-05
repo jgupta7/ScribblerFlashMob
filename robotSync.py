a4285e24c3fac8d16e50ed83bcb669ca92c225c8
Angie Palm <apalm3@gatech.edu>
2014-10-05 17:01:02 -0400

    Gets Unix timestamp
    Uses urllib; iterates through the html code to find the timestamp

--- robotSync.py
+++ robotSync.py
@@ -3,22 +3,37 @@
 
 from Myro import *
 
+import urllib
 import datetime
-import ramblin.py
-import ramblin2.py
+#import ramblin.py
+#import ramblin2.py
 
 #init()
 songOrDance = "dance"
 isComplete = True
+
+
+f = urllib.urlopen("http://www.unixtimestamp.com/index.php")
+stringList = (f.readlines())
+timeStamp = ""
+for string in stringList:
+    count = 0
+    aList = []
+    if "(UTC)" in string:
+        print(string[92:102])
+
 while isComplete:
     currMinute = datetime.datetime.now().time().minute
     if (currMinute == 25):
         #play dat beat
         #do dat dance
         if (songOrDance == "dance"):
-            
-
-        else if (songOrDance == "song"):
+            print("HI")
+        elif (songOrDance == "song"):
             ramblin.ramblin()
             #ramblin2.ramblin()
         isComplete = False
+
+
+#def switchAndSlide():
+

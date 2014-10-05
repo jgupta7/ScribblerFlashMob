759d4d9c0d4c5078db560bbb75cfc6d1c0738db2
jgupta7 <jgupta7@gatech.edu>
2014-10-05 17:09:51 -0400

    Merged File with urllib
    Gets time

--- robotSync.py
+++ robotSync.py
@@ -3,15 +3,14 @@
 
 from Myro import *
 
-import urllib
 import datetime
+import urllib
 import ramblin.py
 import ramblin2.py
 
 #init()
 songOrDance = "dance"
 isComplete = True
-
 
 f = urllib.urlopen("http://www.unixtimestamp.com/index.php")
 stringList = (f.readlines())
@@ -21,3 +20,38 @@ for string in stringList:
     aList = []
     if "(UTC)" in string:
         print(string[92:102])
+
+while isComplete:
+    #currMinute = datetime.datetime.now().time().minute
+    #if (currMinute == 25):
+    #old condition ^ where we use computer time
+    if (words):
+        #play dat beat
+        #do dat dance
+        if (songOrDance == "dance"):
+            
+
+        elif (songOrDance == "song"):
+            ramblin.ramblin()
+            #ramblin2.ramblin()
+        isComplete = False
+
+
+def backAndForth():
+    forward(1,0.5)
+    backward(1,0.5)
+def spinLeftRight():
+    turnLeft(1,0.5)
+    turnRight(1,0.5)
+    turnRight(1,0.5)
+    turnLeft(1,0.5)
+def moveTurnRight():
+    turnRight(1,0.5)
+    forward(1,1)
+    backward(1,1)
+    turnLeft(1,0.5)
+def moveTurnLeft():
+    turnLeft(1,0.5)
+    forward(1,1)
+    backward(1,1)
+    turnRight(1,0.5)
